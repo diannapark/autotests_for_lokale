ce13427e1802b2e47501f9ef0670041631261cea
diannapark <108192842+diannapark@users.noreply.github.com>
2022-07-10 23:33:28 +0300

    Add files via upload
    'Тест проверяет, что страница товара содержит кнопку добавления в корзину'

---
+++ test_items.py
@@ -0,0 +1,10 @@
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+class TestProductPage:
+    
+    def test_button_add_to_basket_is_visible(self, browser):
+
+        browser.get(link)
+
+        time.sleep(5)
+
+        assert browser.find_element_by_css_selector("button.btn-add-to-basket")
